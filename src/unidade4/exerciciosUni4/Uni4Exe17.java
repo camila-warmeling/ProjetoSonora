b8a897589ff31fb1e1a78d2b85c05e4a3110abb3
Camila Warmeling <camila.warmeling04@gmail.com>
2026-04-06 19:18:09 -0300

    Add tax rules comments to Uni4Exe17.java
    enunciado modificado pela estudante para realização da tarefa

--- src/unidade4/exerciciosUni4/Uni4Exe17.java
+++ src/unidade4/exerciciosUni4/Uni4Exe17.java
@@ -2,6 +2,11 @@ package unidade4.exerciciosUni4;
 
 import java.util.Scanner;
 
+//Código realizado segundo a seguinte regra de imposto
+//O contribuinte com uma renda líquida de até R$ 2.000,00 não paga imposto. Para aqueles que possuem renda líquida 
+//entre R$ 2.000,01 e R$ 5.000,00 o imposto é de 5% sobre o valor da renda líquida; para rendas líquidas entre 
+//R$ 5.000,01 e R$ 10.000,00 é de 10%. Rendas superiores a R$ 10.000,01 pagam 15% de imposto.
+
 public class Uni4Exe17 {
 
     Scanner input = new Scanner(System.in);
