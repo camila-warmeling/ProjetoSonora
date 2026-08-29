c009734ae08a09b65073377de6fb44fa22e8b13f
Camila <camila.warmeling04@gmail.com>
2026-08-29 14:02:06 -0300

    feat(utilitarios): adiciona classe Leitor para tratamento de entradas do teclado.

---
+++ src/fase02/utilitarios/Leitor.java
@@ -0,0 +1,27 @@
+package fase02.utilitarios;
+
+import java.util.Scanner;
+
+public class Leitor {
+    private Scanner scanner;
+
+    public Leitor(){
+        this.scanner = new Scanner(System.in);
+    }
+
+    public String lerTexto(String mensagem){
+        System.out.println(mensagem);
+        return scanner.nextLine();
+    }
+
+    public int lerInteiro(String mensagem){
+        while(true){
+            System.out.println(mensagem);
+            try{ //se for possível fazer a conversão o while acaba.
+                return Integer.parseInt(scanner.nextLine()); 
+            }catch(NumberFormatException e){
+                System.out.println("Número inválido. Digite um número inteiro.");
+            }
+        }
+    }
+}
