be559b8d42762c9051c44e194dd78f893e8aef3c
Camila <camila.warmeling04@gmail.com>
2026-09-03 19:22:48 -0300

    feat(PlataformaTest): utilizando @BeforeEach

--- src/fase03/test/PlataformaTest.java
+++ src/fase03/test/PlataformaTest.java
@@ -2,33 +2,32 @@ package fase03.test;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.BeforeEach;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import fase03.Plataforma;
 import fase03.Musica;
 
 public class PlataformaTest {
+    private Plataforma plataforma;
 
-    public Plataforma criarPlataformaComMusicas(){
-        Plataforma plat = new Plataforma();
+    @BeforeEach 
+    public void setup() {
+        plataforma = new Plataforma();
 
-        plat.cadastrarMusica(new Musica("Bohemian Rhapsody", "Queen", 354));
-        plat.cadastrarMusica(new Musica("Song 2", "Blur", 122));
-        plat.cadastrarMusica(new Musica("Hotel California", "Eagles", 390));
-        plat.cadastrarMusica(new Musica("Billie Jean", "Michael Jackson", 294));
-        plat.cadastrarMusica(new Musica("In the End", "Linkin Park", 216));
-
-        return plat;
+        plataforma.cadastrarMusica(new Musica("Bohemian Rhapsody", "Queen", 354));
+        plataforma.cadastrarMusica(new Musica("Song 2", "Blur", 122));
+        plataforma.cadastrarMusica(new Musica("Hotel California", "Eagles", 390));
+        plataforma.cadastrarMusica(new Musica("Billie Jean", "Michael Jackson", 294));
+        plataforma.cadastrarMusica(new Musica("In the End", "Linkin Park", 216));
     }
 
     @Test 
     @DisplayName ("PL06 - Caso 01 - Buscar música cadastrada na plataforma pelo título")
     public void pl06buscarMusicaCadastradaPeloTitulo(){
-        Plataforma plataforma = criarPlataformaComMusicas(); 
-
         Musica musica = plataforma.buscarMusica("Song 2");
         assertNotNull(musica);
         assertEquals("Song 2", musica.getTitulo());
@@ -37,8 +36,6 @@ public class PlataformaTest {
     @Test 
     @DisplayName ("PL06 - Caso 02 - Buscar música não cadastrada na plataforma pelo título")
     public void pl06buscarMusicaNaoCadastradaPeloTitulo(){
-        Plataforma plataforma = criarPlataformaComMusicas(); 
-
         Musica musica = plataforma.buscarMusica("Take On Me");
         assertNull(musica);
     }
@@ -46,8 +43,6 @@ public class PlataformaTest {
     @Test 
     @DisplayName ("PL06 - Caso 03 - Buscar música cadastrada na plataforma pelo id")
     public void pl06buscarMusicaCadastradaPeloId(){
-        Plataforma plataforma = criarPlataformaComMusicas(); 
-
         Musica musica = plataforma.buscarMusica(2);
         assertNotNull(musica);
         assertEquals(2, musica.getId());
@@ -56,8 +51,6 @@ public class PlataformaTest {
     @Test 
     @DisplayName ("PL06 - Caso 04 - Buscar música não cadastrada na plataforma pelo id")
     public void pl06buscarMusicaNaoCadastradaPeloId(){
-        Plataforma plataforma = criarPlataformaComMusicas(); 
-
         Musica musica = plataforma.buscarMusica(10);
         assertNull(musica);
     }
