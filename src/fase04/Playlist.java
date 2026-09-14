ebbf040d46552a9135c77d417203704868014f69
Camila <camila.warmeling04@gmail.com>
2026-09-14 18:29:01 -0300

    feat(Playlist): mudança de vetores de tamanhos fixos para arrayList

--- src/fase04/Playlist.java
+++ src/fase04/Playlist.java
@@ -1,11 +1,12 @@
 package fase04;
 
+import java.util.ArrayList;
+
 public class Playlist {
 
-    private Musica[] playlist = new Musica[100];
+    private ArrayList<Musica> playlist = new ArrayList<>();
     private String nome;
     private Usuario dono; //a variável dono recebe somente objetos da classe Usuario 
-    private int quantMusicas;
 
     public Playlist(String nome, Usuario dono){
         if(nome == null || nome.trim().isEmpty()){
@@ -25,57 +26,48 @@ public class Playlist {
         return this.dono;
     }
 
-    public int getQuantidadeMusicas(){
-        return this.quantMusicas;
-    }
-
     public boolean adicionarMusica(Musica musica){
         if(musica == null){
             throw new IllegalArgumentException("Não é possível adicionar uma música em branco.");
         }
 
-        if(this.quantMusicas < this.playlist.length){
-            playlist[this.quantMusicas] = musica;
-            this.quantMusicas++;
-            return true;
-        }
-        return false;
+        playlist.add(musica);
+        return true;
     }
 
     public Musica getMusicaNaPosicao(int indice){
         if(indice < 0 || indice > 99){
             throw new IndexOutOfBoundsException("Os indices devem estar presentes no intervalo de 0 a 99.");
-        }else if(indice >= this.quantMusicas){//a quantidade de músicas sempre vai ser indice+1. Pois o índice começa no 0 ao invés do 1.
+        }else if(indice >= this.playlist.size()){//a quantidade de músicas sempre vai ser indice+1. Pois o índice começa no 0 ao invés do 1.
             throw new IndexOutOfBoundsException("Ainda não foi adicionado uma música com id " + indice + " nesta playlist.");
         }
-        return this.playlist[indice];
+        return this.playlist.get(indice);
     }
 
     public void removerMusicaNaPosicao(int indice){
         if(indice < 0 || indice > 99){
             throw new IndexOutOfBoundsException("Os indices devem estar presentes no intervalo de 0 a 99.");
-        }else if(indice >= this.quantMusicas){//a quantidade de músicas sempre vai ser indice+1. Pois o índice começa no 0 ao invés do 1.
+        }else if(indice >= this.playlist.size()){//a quantidade de músicas sempre vai ser indice+1. Pois o índice começa no 0 ao invés do 1.
             throw new IndexOutOfBoundsException("Ainda não foi adicionado uma música com id " + indice + " nesta playlist.");
         }
 
-        for(int i=indice; i<this.quantMusicas - 1; i++){
-            this.playlist[i] = this.playlist[i + 1]; //a musica vai ser removida ao ser sobrescrita pela do indice seguinte.
+        for(int i=indice; i<this.playlist.size() - 1; i++){
+            this.playlist.set(i, this.playlist.get(i+1)); //a musica vai ser removida ao ser sobrescrita pela do indice seguinte.
         }
-        this.playlist[this.quantMusicas - 1] = null; //apaga a ultima musica da playlist pois se tornou repetida.
-        this.quantMusicas --;
+        this.playlist.set(this.playlist.size() - 1, null);
     }
 
     public int getDuracaoTotalSegundos(){
         int totalSegundos = 0;
-        for(int i=0; i<this.quantMusicas; i++){
-            totalSegundos += this.playlist[i].getDuracaoSegundos();
+        for(int i=0; i<this.playlist.size(); i++){
+            totalSegundos += this.playlist.get(i).getDuracaoSegundos();
         }
         return totalSegundos;
     }
 
     public void reproduzirTudo(){
-        for(int i=0; i<this.quantMusicas; i++){
-            this.playlist[i].reproduzir();
+        for(int i=0; i<this.playlist.size(); i++){
+            this.playlist.get(i).reproduzir();
         }
     }
 }
