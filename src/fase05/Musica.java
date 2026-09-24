27234d6ab98e646e3f3215d2c5a3840457930bbb
Camila <camila.warmeling04@gmail.com>
2026-09-24 19:52:54 -0300

    feat(Musica): adiciona um album na música.

--- src/fase05/Musica.java
+++ src/fase05/Musica.java
@@ -3,26 +3,40 @@ package fase05;
 public class Musica extends Conteudo{
 
     private String artista;
+    private String album;
     private int reproducoes = 0;
 
 
-    public Musica(String titulo, String artista, int duracaoSegundos){
+    public Musica(String titulo, String artista, int duracaoSegundos, String album){
         super(titulo, duracaoSegundos);
         if (artista == null || artista.trim().isEmpty()){
             throw new IllegalArgumentException("O artista não pode ser vazio.");
         }
-        this.artista  = artista;
+        setArtista(artista);
+        setAlbum(album);
         }
 
     public String getArtista(){
         return this.artista;
     }
 
+    public void setArtista(String artista){
+        this.artista = artista;
+    }
+
+    public String getAlbum(){
+        return this.album;
+    }
+
+    public void setAlbum(String album){
+        this.artista = album;
+    }
+
     public int getReproducoes(){
         return this.reproducoes;
     }
 
-    @Override
+    @Override 
     public void reproduzir(){
         reproducoes ++;
         super.reproduzir();
